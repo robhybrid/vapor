c6cbf49eff813ef9ef92492152c44ee6cfc829cf
Robert Williams <robert.williams@mutualmobile.com>
2014-11-24 21:40:42 -0600

    enable CORS

--- server/config/express.js
+++ server/config/express.js
@@ -27,7 +27,12 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(cookieParser());
-  
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   if ('production' === env) {
     app.use(favicon(path.join(config.root, 'public', 'favicon.ico')));
     app.use(express.static(path.join(config.root, 'public')));
